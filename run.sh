be94f6f662369f9727c4ba96a33704d2f0f9adbd
Conner Mattingly <connermattingly@yahoo.com>
2019-12-29 18:12:02 -0800

    Changed script init proc

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ Description:
 
 Edit History: 
 	DEC 20th 2019 [Initial creation of script. Header created.]	
+	DEC 24th 2019 [Added packages to config.]
 
 COMMENT
 
@@ -17,31 +18,33 @@ COMMENT
 # Global Variables #############################################################
 ################################################################################
 
-PACKAGES_FILENAME=$1
+_package_config_filename="bad_file"
+_bad_config_file_message=$(cat << EndOfMessage 
+ERROR: Either missing package configuration file or file is not a regular file.
+File: ${_package_config_filename}
+EndOfMessage
+)
 
 ################################################################################
 # Function Definitions #########################################################
 ################################################################################
-function Usage() { 
-	echo Script usage
-	echo OS_setup.sh {packages.config}
+function VerifyParams() {
+	if [ ! -f $_package_config_filename  ]; then	
+		echo $_bad_config_file_message
+		echo Exiting.
+		exit 1
+	fi
 }
 
-function VerifyParams() {
-	if [$PACKAGES_FILENAME == ""]; then 
-		echo blah
-	fi 
-
-
-	if [ ! -f $PACKAGES_FILENAME  ]; then	
-		echo 
-		Usage
-	fi
+function UpdateOS() {
+	sudo 
 }
 
 
 ################################################################################
 # Main Script ##################################################################
 ################################################################################
-echo $PACKAGES_FILENAME
-echo Installing packages from 
+echo Running OS Configurer.
+VerifyParams
+UpdateOS
+InstallPackages
